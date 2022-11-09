b87f99600e0176769f4b787fa48e327a7497ff21
c1tr00z <c1tr00z@yandex.ru>
2022-11-09 21:33:32 +0900

    Connecting plugin to project

---
+++ Source/CardGamePrototypeUE/Core/CGPGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Core/AssistLibGameInstance.h"
+#include "CGPGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CARDGAMEPROTOTYPEUE_API UCGPGameInstance : public UAssistLibGameInstance
+{
+	GENERATED_BODY()
+	
+};
