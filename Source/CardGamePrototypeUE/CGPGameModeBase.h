b6f8831a491abb730d2521e94f9911bb1f395ecb
c1tr00z <c1tr00z@yandex.ru>
2022-11-09 18:48:37 +0900

    Initial commit

---
+++ Source/CardGamePrototypeUE/CGPGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CGPGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CARDGAMEPROTOTYPEUE_API ACGPGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
